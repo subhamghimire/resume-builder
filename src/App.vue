41619d60f94fb78589254ea06627d290ff496229
subhamghimire <subham.ghimire26@gmail.com>
2022-03-21 19:52:18 +0545

    Project init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+nav {
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>Home Page</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomePage",
+  components: {},
+  setup() {
+    return {};
+  },
+});
+</script>
